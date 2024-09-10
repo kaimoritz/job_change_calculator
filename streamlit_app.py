d6f901fc35197ee6a0134649cf061a55bac6d0bd
Kai Moritz <kmoritz@telekom.de>
2024-09-10 15:11:37 +0200

    bug fix final salary metric, renaming to compensation payment, Note with links to online calcs.

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
     menu_items={
-        'Get Help': 'https://github.com/kaimoritz/job_change_calculator',
+        'Get Help': "https://github.com/kaimoritz/job_change_calculator",
         'Report a bug': "https://github.com/kaimoritz/job_change_calculator",
         'About': "Version 1.0 | Created by Kai Moritz."
     }
@@ -17,8 +17,8 @@ st.set_page_config(
 TYPE_OF_INCOME = "Type of income"
 NEW_JOB = "Salary new job"
 CURRENT_JOB = "Salary current Job"
-ANNUAL_SEVERANCE_PAY = "Severance pay (annual)"
-TOTAL_NEW_JOB = f"Total: {NEW_JOB} + {ANNUAL_SEVERANCE_PAY}"
+ANNUAL_COMPENSATIOIN = "Compensation payment (annual)"
+TOTAL_NEW_JOB = f"Total: {NEW_JOB} + {ANNUAL_COMPENSATIOIN}"
 
 st.logo("images/logo.png")
 st.sidebar.text("")  # vertical space
@@ -41,24 +41,24 @@ salary_increase_input = st.sidebar.number_input("Expected annual salary increase
                                                 help=help_salary_increase_input)
 salary_increase_rate = salary_increase_input / 100
 
-severance_pay = 0
-severance_annual_rate = 0
-help_severance_paid= "If you will receive a severance pay, activate the checkbox and enter your numbers."
-severance_paid = st.sidebar.checkbox("I will receive a severance pay", help=help_severance_paid)
-if severance_paid:
-    help_severance_pay="Enter the amount of severance pay you will receive. You can enter your gross or net salary, but then enter gross or net everywhere."
-    severance_pay = st.sidebar.number_input("Severance pay (k€)", min_value=0, value=50, step=1, help=help_severance_pay)
-    help_severance_annual_pay="You can use the severance pay you receive to compensate for a lower salary in your new job. To do this, you pay yourself the desired amount of your severance pay each year."
-    severance_annual_rate = st.sidebar.number_input("Annual payment from severance pay (yearly/k€)",
-                                                    min_value=0,
-                                                    value=10,
-                                                    help=help_severance_annual_pay)
+compensation_payment = 0
+compensation_annual_rate = 0
+help_compensation_paid= "If you will receive a compensation payment, activate the checkbox and enter your numbers."
+compensation_paid = st.sidebar.checkbox("I will receive a compensation payment", help=help_compensation_paid)
+if compensation_paid:
+    help_compensation_payment= "Enter the amount of compensation payment you will receive. You can enter your gross or net salary, but then enter gross or net everywhere."
+    compensation_payment = st.sidebar.number_input("Compensation payment (k€)", min_value=0, value=50, step=1, help=help_compensation_payment)
+    help_compensation_annual_payment= "You can use the compensation payment you receive to compensate for a lower salary in your new job. To do this, you pay yourself the desired amount of your compensation payment each year."
+    compensation_annual_rate = st.sidebar.number_input("Annual payment from compensation payment (yearly/k€)",
+                                                       min_value=0,
+                                                       value=10,
+                                                       help=help_compensation_annual_payment)
 
 st.sidebar.text("") # vertical space
 st.sidebar.write("<sup>Reset: Press 'STRG'+'F5'</sup>", unsafe_allow_html=True)
 
 def calculate_final_salary(start_salary, increase, years):
-    final_salary = start_salary * (1 + increase) ** years
+    final_salary = start_salary * (1 + increase) ** (years -1) # years-1 because the first increase is wfter the first year.
     return final_salary
 
 
@@ -88,10 +88,10 @@ def add_calculations_salary_in_the_next_years(df, name, initial_salary, salary_i
     return df
 
 
-def add_severance_payments(df, severance_pay, annual_payment) -> pd.DataFrame:
+def add_compensation_payments(df, compensation_payment, annual_payment) -> pd.DataFrame:
     # Calculation of payments
     payments = []
-    remaining_balance = severance_pay
+    remaining_balance = compensation_payment
 
     for year in column_names:
         if year == column_names[-1]:
@@ -105,10 +105,10 @@ def add_severance_payments(df, severance_pay, annual_payment) -> pd.DataFrame:
                 remaining_balance = 0
 
     # Add new row to DataFrame
-    df.loc[ANNUAL_SEVERANCE_PAY] = payments
+    df.loc[ANNUAL_COMPENSATIOIN] = payments
 
-    # add new row for new job + severance
-    df.loc[TOTAL_NEW_JOB] = np.add(df.loc[NEW_JOB], df.loc[ANNUAL_SEVERANCE_PAY])
+    # add new row for new job + compensation
+    df.loc[TOTAL_NEW_JOB] = np.add(df.loc[NEW_JOB], df.loc[ANNUAL_COMPENSATIOIN])
 
     return df
 
@@ -118,8 +118,8 @@ df = pd.DataFrame(columns=column_names)
 df.index.name = TYPE_OF_INCOME
 df = add_calculations_salary_in_the_next_years(df, CURRENT_JOB, current_job_salary, salary_increase_rate)
 df = add_calculations_salary_in_the_next_years(df, NEW_JOB, new_job_salary, salary_increase_rate)
-if severance_paid:
-    df = add_severance_payments(df, severance_pay, severance_annual_rate)
+if compensation_paid:
+    df = add_compensation_payments(df, compensation_payment, compensation_annual_rate)
 
 df.columns = df.columns.astype(float)
 df = df.sort_index(axis=1)
@@ -139,7 +139,10 @@ col_1_1.metric(f"Starting salary new job",
                delta=f"{new_job_salary - current_job_salary:.2f} k€",
                help=help_salary_new_job_initial)
 if salary_increase_rate > 0.0:
-    help_salary_new_job_final = f"This is the salary of the new job when you retire. This takes into account the 'Expected annual salary increase rate (%)' of {salary_increase_rate}%. Compared to the current job's salary when you retire."
+    help_salary_new_job_final = (f"This is the salary of the new job when you retire ({new_job_salary_final:.2f} k€). "
+                                 f"This takes into account the 'Expected annual salary increase rate (%)' of "
+                                 f"{salary_increase_rate}%. Compared to the current job's salary "
+                                 f"({(current_job_salary_final):.2f} k€) when you retire.")
     col_1_2.metric(f"Salary new job in {years} years",
                    value=f"{new_job_salary_final:.2f} k€",
                    delta=f"{(new_job_salary_final - current_job_salary_final):.2f} k€",
@@ -152,21 +155,22 @@ col_1_3.metric("Overall sum current job",
                value=f"{current_job_overall_salary:.2f} k€",
                help=help_overall_sum_current)
 
-help_overall_sum_new = "The sum of your new job's salary until your retirement. Does not include the severance pay."
+help_overall_sum_new = "The sum of your new job's salary until your retirement. Does not include the compensation payment."
 new_job_overall_salary = overall_sum_4_job(df, NEW_JOB, column_names)
 col_1_4.metric("Overall sum new job",
                value=f"{new_job_overall_salary:.2f} k€",
                help=help_overall_sum_new
                )
 
-help_severance = "Amount of your severance pay, if received"
+help_compenstation = "Amount of your compensation payment, if received"
 col_1_5.metric("Severance pay",
-               value=f"{severance_pay:.2f} k€",
-               help=help_severance
+               value=f"{compensation_payment:.2f} k€",
+               help=help_compenstation
                )
 
-overall_delta = new_job_overall_salary + severance_pay - current_job_overall_salary
-help_overall_delta = f"The is the cumulative income for the next {years} years, including the severance pay of {severance_pay} k€. New job+severance pay vs. current job."
+overall_delta = new_job_overall_salary + compensation_payment - current_job_overall_salary
+help_overall_delta = (f"The is the cumulative income for the next {years} years, including the compensation payment of "
+                      f"{compensation_payment} k€. New job+compensation payment vs. current job.")
 col_1_6.metric("Overall delta",
                value=f"{overall_delta:.2f} k€",
                delta=f"{overall_delta:.2f} k€",
@@ -227,8 +231,10 @@ column_config[df.index.name] = st.column_config.TextColumn(width=220)
 
 st.dataframe(df, column_config=column_config)
 
-st.text("""
-Note: I have deliberately not taken gross/net salary into account here, as this is highly individual. You may enter your 
-gross or net salary, see online gross/net calculators to get your numbers.
-"""
-        )
+st.write("""Note: I have deliberately not taken gross/net salary into account here, as this is highly individual. You 
+may enter your your gross or net salary, but you should than stick to one type - don't  mix it up. Check out a <a 
+href='https://www.lexware.de/werkzeuge-ebooks/brutto-netto-rechner/' id='gross-net-link'>online gross/net 
+calculators</a> to get your numbers. In particular, the calculation of the net amount of the compensation payment 
+must be considered separately. For more details see <a 
+href='https://www.lexware.de/werkzeuge-ebooks/abfindungsrechner/' id='gross-net-link'> online compensation payment 
+calculators</a> """, unsafe_allow_html=True)
