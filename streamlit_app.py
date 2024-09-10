63f03574dc4a0c43a81484379fb8b8f57b3bbe15
Kai Moritz <kmoritz@telekom.de>
2024-09-10 06:33:12 +0200

    layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,22 +19,35 @@ st.logo("images/logo.png")
 st.sidebar.text("")  # vertical space
 
 # sidebar: parameter input
-years = st.sidebar.number_input("Years until my retirement", min_value=0, max_value=100, value=5, step=1)
-current_job_salary = st.sidebar.number_input("Current Job Salary (k€/year)", min_value=0, value=100)
-new_job_salary = st.sidebar.number_input("New job Salary (k€/year)", min_value=0, value=90)
+help_year="Enter the number of years you want work until retirement."
+years = st.sidebar.number_input("Years until my retirement", min_value=0, max_value=100, value=5, step=1, help=help_year)
+
+help_current_job_salary = "Enter the salary of your CURRENT job. You can enter your gross or net salary, but then enter gross or net everywhere."
+current_job_salary = st.sidebar.number_input("Current Job Salary (k€/year)", min_value=0, value=100, help=help_current_job_salary)
+
+help_new_job_salary = "Enter the salary of your NEW job. You can enter your gross or net salary, but then enter gross or net everywhere."
+new_job_salary = st.sidebar.number_input("New job Salary (k€/year)", min_value=0, value=90, help=help_new_job_salary)
+
+help_salary_increase_input="Enter the expected annual salary increase in percent. E.g. '2.00'%."
 salary_increase_input = st.sidebar.number_input("Expected yearly salary increase (%)",
                                                 min_value=0.0,
-                                                value=2.0,
-                                                step=0.1)
+                                                value=0.0,
+                                                step=0.1,
+                                                help=help_salary_increase_input)
 salary_increase_rate = salary_increase_input / 100
 
 severance_pay = 0
 severance_annual_rate = 0
-severance = st.sidebar.checkbox("I received a severance pay")
-if severance:
-    severance_pay = st.sidebar.number_input("Severance pay (k€)", min_value=0, value=50, step=1)
-    severance_annual_rate = st.sidebar.number_input("Annual payment from severance pay (yearly/k€)", min_value=0, value=10)
-st.write(severance_pay, severance_annual_rate)
+help_severence_paid="If you will receive a severence pay, activate the checkbox and enter your numbers."
+severance_paid = st.sidebar.checkbox("I will receive a severance pay", help=help_severence_paid)
+if severance_paid:
+    help_severance_pay="Enter the amount of severance pay you will receive. You can enter your gross or net salary, but then enter gross or net everywhere."
+    severance_pay = st.sidebar.number_input("Severance pay (k€)", min_value=0, value=50, step=1, help=help_severance_pay)
+    help_severance_annual_pay="You can use the severance pay you receive to compensate for a lower salary in your new job. To do this, you pay yourself the desired amount of your severance pay each year."
+    severance_annual_rate = st.sidebar.number_input("Annual payment from severance pay (yearly/k€)",
+                                                    min_value=0,
+                                                    value=10,
+                                                    help=help_severance_annual_pay)
 
 
 def calculate_final_salary(start_salary, increase, years):
@@ -88,19 +101,18 @@ def add_severance_payments(df, severance_pay, annual_payment) -> pd.DataFrame:
     df.loc["Severance payment"] = payments
 
     # add new row for new job + severance
-    df.loc["Total new job and severance"] = np.add(df.loc["New job"], df.loc["Severance payment"])
+    df.loc["Total new job + annual severance"] = np.add(df.loc["New job"], df.loc["Severance payment"])
 
     return df
 
 
-column_names = [float(i) for i in range(years + 1)]
+column_names = [float(i) for i in range(years)]
 df = pd.DataFrame(columns=column_names)
+df.index.name = "Type of income"
 df = add_calculations_salary_in_the_next_years(df, "Current job", current_job_salary, salary_increase_rate)
 df = add_calculations_salary_in_the_next_years(df, "New job", new_job_salary, salary_increase_rate)
-df = add_severance_payments(df, severance_pay, severance_annual_rate)
-
-#df_severance = pd.DataFrame(columns=column_names)
-#df_severance = add_severance_payments(df_severance, severance_pay, severance_annual_rate)
+if severance_paid:
+    df = add_severance_payments(df, severance_pay, severance_annual_rate)
 
 df.columns = df.columns.astype(float)
 df = df.sort_index(axis=1)
@@ -113,7 +125,7 @@ def overall_sum_4_job(df, job_name, column_names) -> float:
 
 
 st.header("Key metrics")
-col_1_1, col_1_2, col_1_3, col_1_4, col_1_5 = st.columns(5)
+col_1_1, col_1_2, col_1_3, col_1_4, col_1_5, col_1_6 = st.columns(6)
 help_salary_new_job_initial = "Your start salary of the new job. With a comparision to your current's job salary."
 col_1_1.metric(f"Salary new job inital",
                value=f"{new_job_salary} k€",
@@ -137,14 +149,24 @@ col_1_4.metric("Overall sum new job",
                value=f"{new_job_overall_salary:.2f} k€",
                help=help_overall_sum_new
                )
-overall_difference = new_job_overall_salary - current_job_overall_salary
-help_overall_delta = f"The is the cumulative salary for the next {years} years. New job vs. current job."
-col_1_5.metric("Overall delta",
-               value=f"{overall_difference:.2f} k€",
+
+help_severance = "Amount of your severance pay, if received"
+col_1_5.metric("Severance pay",
+               value=f"{severance_pay:.2f} k€",
+               help=help_severance
+               )
+
+overall_delta = new_job_overall_salary + severance_pay - current_job_overall_salary
+help_overall_delta = f"The is the cumulative income for the next {years} years, including the severance pay of {severance_pay} k€. New job+severance pay vs. current job."
+col_1_6.metric("Overall delta",
+               value=f"{overall_delta:.2f} k€",
+               delta=f"{overall_delta:.2f} k€",
                help=help_overall_delta)
 
-col_3_1, col_3_2, col_3_3, col_3_4 = st.columns([2, 1, 1, 1])
-col_3_1.header("Future development")
+
+
+col_3_1, col_3_2, col_3_3, col_3_4 = st.columns([2, 1, 1, 2])
+col_3_1.header("Future development of your income")
 
 with col_3_2:
     data_type = st.radio(
@@ -182,18 +204,19 @@ st.header("Detailed calculation")
 
 # column configs:
 # create a number float %.1f only for dtype float (-> dynamic names "relevant month)
-column_config_float = {}
+column_config = {}
 for col in column_names:
-    column_config_float[col] = st.column_config.NumberColumn(
+    column_config[col] = st.column_config.NumberColumn(
         f"+ {int(col)} years",
         help=f"Your future income in {int(col)} years.",
         min_value=0,
         # step=1,
         format="%.2f k€",
     )
+# add column config for index / "type"
+column_config[df.index.name] = st.column_config.TextColumn(width=220)
 
-st.dataframe(df, column_config=column_config_float
-             )
+st.dataframe(df, column_config=column_config)
 
 st.text("""
 Note: I have deliberately not taken gross/net salary into account here, as this is highly individual. You may enter your 
